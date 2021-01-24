90ce5a9e8559fc53ae074f97e37364acd5140870
Yu Shi <yushiys95@gmail.com>
2021-01-24 04:50:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,10 @@ end	end
 
 def get_english_meaning
   # code goes here
-  emoticon_lib = load_library(emoticon_file)
-  english_meaning = emoticon_lib['get_meaning'][emoticon]
-  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
-end	
+  lookup = load_library(path)
+  if lookup[:get_meaning][emoticon]
+    return lookup[:get_meaning][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
+end 
